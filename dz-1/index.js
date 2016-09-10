71cad6f612f09bc407b26e1a18038afc8856f1da
ol-bashkin <ol.bashkin@gmail.com>
2016-09-10 08:49:20 +0300

    Домашнее задание

--- dz-1/index.js
+++ dz-1/index.js
@@ -1,3 +1,7 @@
 const ansi = require("ansi");
+const colors = require("colors");
 const cursor = ansi(process.stdout);
+
+console.log('HELLO, MY FRIEND!'.rainbow);
+console.log('Who would imagine this?'.trap);
 cursor.beep();
